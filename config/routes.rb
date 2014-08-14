c66a8ef12555fff5223f22874f29e65e3f3b2257
Josh Cornelius <joshcornelius9@gmail.com>
2014-08-14 09:12:41 -0400

    added welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
+  root to: 'welcome#index'
 end
